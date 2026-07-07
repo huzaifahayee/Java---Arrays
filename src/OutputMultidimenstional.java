9c1d86290d1390f2074bf1c57de28d27edaef4a7
Muhammad Huzaifa <huzaifahayee751@gmail.com>
2026-07-07 18:10:13 +0500

    2D array output

---
+++ src/OutputMultidimenstional.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class OutputMultidimenstional {
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][4];
+
+        Scanner sc = new Scanner(System.in);
+        //----------Input-------------
+
+        // for every row
+        for (int row = 0; row < arr.length; row++) {
+
+            //for every column
+            for (int column = 0; column < arr[row].length; column++) {
+                System.out.print("Enter arr[" + row + "][" + column+"] : ");
+                arr[row][column] = sc.nextInt();
+            }
+
+        }
+
+
+        //----Output ---------
+
+
+        for (int row = 0; row < arr.length; row++) {
+
+            //for every column
+            for (int column = 0; column < arr[row].length; column++) {
+                System.out.print(arr[row][column] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
